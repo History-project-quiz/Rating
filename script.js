b262a58d945f7e6d2fdac7c1eaf656648742e203
maksim-mshp <maksim-mshp@ya.ru>
2022-07-17 12:05:42 +0300

    подгрузка сразу после открытия страницы

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-'use strict';
-let ws1 = new WebSocket("wss://api.tinkoffgame.ml/ws?chat_id=-1001729919564&game_id=1");
+"use strict";
 
-ws1.onmessage = function(event) {
+function show(element, data) {
   let s = "";
-  JSON.parse(event.data).forEach(i => {
-    s += `<li>${i.name} – ${i.score}</li>`;
-  });
-  document.querySelector('#game1').innerHTML = s;
+	data.forEach((i) => {
+		s += `<li>${i.name} – ${i.score}</li>`;
+	});
+  document.querySelector(element).innerHTML = s;
+}
+
+fetch("https://api.tinkoffgame.ml/api/v1/table?chat_id=-1001729919564&game_id=1")
+  .then(response => response.text())
+  .then(result => {
+    show("#game1", JSON.parse(result).table);
+  })
+
+let ws1 = new WebSocket(
+	"wss://api.tinkoffgame.ml/ws?chat_id=-1001729919564&game_id=1"
+);
+
+ws1.onmessage = function (event) {
+	show("#game1", JSON.parse(event.data));
 };
